4ba1fa8bd16958b925de52848ef16a9903762223
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-14 18:40:01 +0300

    Create Task2.3_CountArr.js

---
+++ task2/Task2.3_CountArr.js
@@ -0,0 +1,14 @@
+function CountArr (arr) {
+	var even = 0;
+	var odd = 0;
+	var zero = 0;
+	for ( var val of arr ) {
+		if ( val == 0){
+			zero+=1; }
+			else { (val % 2 != 0) ? odd+=1 : even+=1;}
+			}
+			console.log("четных: "+even, "нечетных: "+odd, "нулей: "+zero);
+			return [even, odd, zero]
+				
+		}//CountArr
+console.log(CountArr([2,3,4,5,6,0]));
