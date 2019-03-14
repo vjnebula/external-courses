2461ea685e6f1e636e8a63402875417ebdd2af21
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-14 19:53:20 +0300

    Update Task2.1_NumString.js

--- task2/Task2.1_NumString.js
+++ task2/Task2.1_NumString.js
@@ -10,4 +10,14 @@ function NumStr (b){
 function NumStr2 (c){
     return ((typeof(c) == "number")||(typeof(c) == "string")) ? typeof(c) : undefined;
     }
-alert(NumStr(prompt("Enter argument","")));
+console.log(NumStr("sgsdf"));
+console.log(NumStr(2345));
+console.log(NumStr2("sgsdf"));
+console.log(NumStr2(2345));
+console.log(NumStr(true));
+console.log(NumStr2(false));
+console.log(NumStr(undefined));
+console.log(NumStr2(null));
+console.log(NumStr2(NaN));
+
+end = "end of script";
