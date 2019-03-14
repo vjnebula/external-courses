cf2c77d82471927e103dec832a8dc2d3852a1cd1
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-14 19:53:15 +0300

    Update Task2.2_LogArray.js

--- task2/Task2.2_LogArray.js
+++ task2/Task2.2_LogArray.js
@@ -2,5 +2,8 @@ function LogArray (Arr){
     for (var val of Arr){
         console.log(val);
     }
-    console.log(Arr.length);
+    console.log("Длинна массива "+Arr.length);
 }
+LogArray("Ann, Bill, Alice, Marry, Klod, Pol, Jack".split(", "));
+
+end = "end of script";
