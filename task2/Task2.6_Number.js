e94523723a84cc982bc18b720d357601a47481d5
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-14 19:52:46 +0300

    Create Task2.6_Number.js

---
+++ task2/Task2.6_Number.js
@@ -0,0 +1,10 @@
+function CheckNum (num) {
+	if ( num <= 1000 ) {
+		(num % 2 != 0) ? alert("простое") : alert("составное");
+	}
+	else (alert( "wrong number" ));
+}//CheckNum()
+
+CheckNum(prompt("Enter number",""))
+
+end = "end of script";
