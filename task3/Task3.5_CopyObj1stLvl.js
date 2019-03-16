b715a6ed4540b18890f826fb4065141bbfb603e9
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-16 21:50:18 +0300

    Update Task3.5_CopyObj1stLvl.js

--- task3/Task3.5_CopyObj1stLvl.js
+++ task3/Task3.5_CopyObj1stLvl.js
@@ -3,8 +3,8 @@ return ( obj instanceof Object ) }
 
 function CopyObjLink (obj) { 
 	if ( CheckObj(obj) ) {
-		var newobj = obj;
-		//var newobj = Object.assign({},obj);
+		//var newobj = obj;
+		var newobj = Object.assign({},obj);
 		return newobj; 
 	}//if CheckObj
 	else console.log("not and object");
@@ -14,5 +14,8 @@ var object2 = { prpO2: "string2", prp2O2: object1 };
 var object3 = { prpO3: "string2", prp2O3: object2 };
 //console.log( CopyObjLink ( "test" ));
 //console.log( CopyObjLink (object1) );
-console.log(CopyObjLink(object3));
+console.log( CopyObjLink(object3) );
+console.log( CopyObjLink(object3) == object3 );
+console.log( CopyObjLink(object3.prp2O3) == object3.prp2O3);
+console.log( CopyObjLink(object3.prp2O3.prp2O2) == object3.prp2O3.prp2O2);
 end = "end of script";
