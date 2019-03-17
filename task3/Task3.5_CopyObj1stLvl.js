e9bd877c92c581c18b714e56fb78b115b3b79608
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-17 19:27:22 +0300

    Update Task3.5_CopyObj1stLvl.js

--- task3/Task3.5_CopyObj1stLvl.js
+++ task3/Task3.5_CopyObj1stLvl.js
@@ -3,19 +3,40 @@ return ( obj instanceof Object ) }
 
 function CopyObjLink (obj) { 
 	if ( CheckObj(obj) ) {
-		//var newobj = obj;
+		
 		var newobj = Object.assign({},obj);
 		return newobj; 
 	}//if CheckObj
 	else console.log("not and object");
 }//func CopyObjLink
+
+function CopyObjLink2 (obj) { 
+	if ( CheckObj(obj) ) {
+		var newobj = obj;
+		
+		return newobj; 
+	}//if CheckObj
+	else console.log("not and object");
+}//func CopyObjLink
+
 var object1 = { prpO1: "string1", prp2O1: 1111 };
 var object2 = { prpO2: "string2", prp2O2: object1 };
 var object3 = { prpO3: "string2", prp2O3: object2 };
-//console.log( CopyObjLink ( "test" ));
-//console.log( CopyObjLink (object1) );
-console.log( CopyObjLink(object3) );
-console.log( CopyObjLink(object3) == object3 );
-console.log( CopyObjLink(object3.prp2O3) == object3.prp2O3);
-console.log( CopyObjLink(object3.prp2O3.prp2O2) == object3.prp2O3.prp2O2);
+
+console.log( CopyObjLink ( "must be object" ));
+
+var TestObj = CopyObjLink(object3);//newobj = Object.assign({},obj)
+
+console.log( TestObj );
+console.log( TestObj === object3 );
+console.log( TestObj.prp2O3 === object3.prp2O3);
+console.log( TestObj.prp2O3.prp2O2 === object3.prp2O3.prp2O2);
+
+var TestObj = CopyObjLink2(object3);//newobj = obj
+
+console.log( TestObj );
+console.log( TestObj === object3 );
+console.log( TestObj.prp2O3 === object3.prp2O3);
+console.log( TestObj.prp2O3.prp2O2 === object3.prp2O3.prp2O2);
 end = "end of script";
+
