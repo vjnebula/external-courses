09033cc968e9ee9c4240f216f7ac5cee796a5676
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-19 22:16:35 +0300

    Update Task3.5_CopyObj1stLvl.js

--- task3/Task3.5_CopyObj1stLvl.js
+++ task3/Task3.5_CopyObj1stLvl.js
@@ -3,21 +3,16 @@ return ( obj instanceof Object ) }
 
 function CopyObjLink (obj) { 
 	if ( CheckObj(obj) ) {
-		
-		var newobj = Object.assign({},obj);
-		return newobj; 
+		var newobj = {};
+		for ( var prop in obj ) {
+			
+				newobj[prop] = obj[prop]; 
+		}//for 
+		return newobj;
 	}//if CheckObj
 	else console.log("not and object");
 }//func CopyObjLink
 
-function CopyObjLink2 (obj) { 
-	if ( CheckObj(obj) ) {
-		var newobj = obj;
-		
-		return newobj; 
-	}//if CheckObj
-	else console.log("not and object");
-}//func CopyObjLink
 
 var object1 = { prpO1: "string1", prp2O1: 1111 };
 var object2 = { prpO2: "string2", prp2O2: object1 };
@@ -32,11 +27,5 @@ console.log( TestObj === object3 );
 console.log( TestObj.prp2O3 === object3.prp2O3);
 console.log( TestObj.prp2O3.prp2O2 === object3.prp2O3.prp2O2);
 
-var TestObj = CopyObjLink2(object3);//newobj = obj
-
-console.log( TestObj );
-console.log( TestObj === object3 );
-console.log( TestObj.prp2O3 === object3.prp2O3);
-console.log( TestObj.prp2O3.prp2O2 === object3.prp2O3.prp2O2);
 end = "end of script";
 
