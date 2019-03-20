40c0a69e3cbe80a414f993819f1aec586e521b66
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-20 20:20:01 +0300

    Create Task4.6_UpercaseEachWord.js

---
+++ task4/Task4.6_UpercaseEachWord.js
@@ -0,0 +1,23 @@
+function ToUpCaseEach(str) {
+	if ( typeof str == "string" ) {
+		
+		for (i = 0; i < str.length; i++){
+		
+		if ( str[i] != " " && "1234567890".indexOf(str[i]) < 0 ){
+			if ( str[i-1] == " " || "1234567890".indexOf(str[i-1]) >= 0 ){
+			str = str.slice( 0, i ) + str[i].toUpperCase() + str.slice( i + 1, str.length ) ;
+			}
+			
+		}
+		
+		}//for i
+		
+		return str;
+	}//if typeof
+	else console.log ("not a string");
+}// func RemoveSpaces
+
+console.log(ToUpCaseEach("1  2sdf asd ggg zff"));
+
+
+end = "end of script";
