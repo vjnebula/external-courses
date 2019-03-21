a08c26944342fcf609e10ebff880d01019887a04
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-21 21:13:11 +0300

    Update Task4.13_Rand.js

--- task4/Task4.13_Rand.js
+++ task4/Task4.13_Rand.js
@@ -1,5 +1,8 @@
 function Rand (min, max) {
+		max = max +1;
 		return Math.random() * (max - min) + min;
 } //func Rand
 
-console.log(Rand (0, 100));
+console.log( Rand(0, 100) );
+
+end = "end of script";
