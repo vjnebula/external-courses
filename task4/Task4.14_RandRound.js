474fa6586a353d06b57261e1cbf9fe3ce74f4119
vjnebula <48429625+vjnebula@users.noreply.github.com>
2019-03-23 21:26:48 +0300

    Update Task4.14_RandRound.js

--- task4/Task4.14_RandRound.js
+++ task4/Task4.14_RandRound.js
@@ -1,10 +1,18 @@
+/*
 function Rand (min, max) {
 		max = max + 1;
 		var num = (Math.random() * (max - min) + min).toString().split(".");
 		return +num[0];
+} //func Rand 
+*/
+
+function Rand2 (min, max) {
+		max = max +1;
+		result = Math.random() * (max - min) + min;
+		return Math.floor(result);
 } //func Rand
 
-console.log(Rand (99, 100));
-console.log(typeof Rand (0, 100));
-console.log(Rand (0, 100));
+console.log(Rand2 (99, 100));
+//console.log(typeof Rand (0, 100));
+console.log(Rand2 (0, 100));
 end = "end of script";
