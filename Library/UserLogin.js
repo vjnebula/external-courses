fff138d359922fc2e68fd5faf8994dd40c82dc87
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-15 21:36:17 +0300

    Update UserLogin.js

--- Library/UserLogin.js
+++ Library/UserLogin.js
@@ -3,7 +3,8 @@ function drp() {
 	for (var e of dropElements) {
 		e.style.display = 'block';
 	}//for e
-	
+	var myButton = document.getElementById('signIn');
+	myButton.onclick = userSignIn;
 	myDiv.onclick = drpClose;
 }//drp
 
@@ -15,6 +16,17 @@ function drpClose() {
 	}//for e
 }//dropClose
 
+function userSignIn() {
+	var dropElements = document.getElementsByClassName('dropdown-content');
+	//myDiv.onclick = drp;
+	for (var e of dropElements) {
+		e.style.display = 'none';
+	}//for e
+	myDiv.onclick = "";
+	myDiv.style.display = 'none';
+	var userMenu = document.getElementById('user_logged');
+	userMenu.style.display = 'flex';
+}//userSignIn
 
 var myDiv = document.getElementById('user');
 myDiv.onclick = drp;
