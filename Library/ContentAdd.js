0c9beeaae22e270ccb43094f86dce0dfa972cf80
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-21 05:55:43 +0300

    Update ContentAdd.js

--- Library/ContentAdd.js
+++ Library/ContentAdd.js
@@ -87,27 +87,7 @@ var starArr = document.getElementsByClassName('SVGstars');
 		s.onmouseout = clear;
 		s.onclick = setRate;
 	} 
+
+var histArr = [];
+//histArr.length = 10; 
  
- 
-/* var content_prnt_div = document.getElementById('main_content');
-	var bk_title = "Cakes & Bakes";
-	var bk_author = "by Sanjeev Kapoor";
-	var newDiv = document.createElement("div");
-	newDiv.innerHTML = "<div><img src='book/Cakes & Bakes by Sanjeev Kapoor.png'></img></div>" + 
-		"<div class='book_title'>" + bk_title + "</div>" + 
-		"<div class='book_author'>" + bk_author + "</div>" + 
-		"<div id='stars_parent'>" + 
-			"<svg class='stars' width='75' height='15' viewBox='0 0 75 15'>" + 
-				"<use x='00' class='SVGstars' xlink:href='#starSVG' />" + 
-				"<use x='15' class='SVGstars' xlink:href='#starSVG' />" +
-				"<use x='30' class='SVGstars' xlink:href='#starSVG' />" + 
-				"<use x='45' class='SVGstars' xlink:href='#starSVG' />" + 
-				"<use x='60' class='SVGstars' xlink:href='#starSVG' />"+
-			"</svg>"+
-		"</div>";
-	newDiv.className = "m_content";
-	content_prnt_div.appendChild(newDiv);
-	//newDiv = document.createElement('div');
-	//var tempText = document.createTextNode("Jewels of Nizam");
-	//newDiv.appendChild(tempText);
-	//document.content_div.appendChild(newDiv); */
