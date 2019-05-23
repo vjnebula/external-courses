ff68f397954111b2b497a39d606e491e42ee3388
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-23 17:58:16 +0300

    Create library.controller.js

---
+++ Library/controllers/library.controller.js
@@ -0,0 +1,41 @@
+document.getElementById('file-input').onchange = getfiles;
+
+document.getElementById('f_recent').onclick = fltrRecent;
+document.getElementById('f_favourite').onclick = fltrFavor;
+document.getElementById('f_nowReading').onclick = fltrNowReading;
+document.getElementById('f_free').onclick = fltrFree;
+document.getElementById('f_best').onclick = fltrBest;
+document.getElementById('f_all').onclick = fltrAll;
+document.getElementById('m_browse').onclick = fltrAll;
+document.getElementById('f_non_fiction').onclick = fltrNonFiction;
+document.getElementById('f_classic').onclick = fltrClassic;
+document.getElementById('f_must').onclick = fltrMust;
+document.getElementById('f_popular').onclick = fltrPopular;
+document.getElementById('m_history').onclick = fltrHistory;
+
+var myDiv = document.getElementById('user');
+myDiv.onclick = drp;
+
+function fltrMust() {
+	fltrCat("must_read");
+	newHistMesAdd("You used Must Read filter");
+}
+
+function fltrBest() {
+	fltrCat("best");
+	newHistMesAdd("You used Best filter");
+}
+
+function fltrClassic() {
+	fltrCat("classic");
+	newHistMesAdd("You used Classic filter");
+}
+
+function fltrNonFiction() {
+	fltrCat("non_fiction");
+	newHistMesAdd("You used Non Fiction filter");
+}
+
+function fltrNowReading() {
+	fltrRecent();//with updatedAt field, no mater because of random
+}//fltrNowReading
