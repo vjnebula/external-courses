acfd37d706ea93445fae963b752d220d80d7d12e
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-24 16:32:32 +0300

    Update search.controller.js

--- Library/controllers/search.controller.js
+++ Library/controllers/search.controller.js
@@ -6,7 +6,7 @@ s_input.addEventListener("search", function() {
 		newHistMesAdd("You has searched for " + this.value);
 		search_myLib(this.value);
 		} else {
-			fltrAll();  
+			fltrAll();
 		}
 	
 });
@@ -15,12 +15,12 @@ keyup();//initialize search field
 
 function search_myLib(s_str) {
 
-	for (var b of myLib) {
-			var cur_book = document.getElementById(b.id);
+	for (var b in myLib) {
+			var cur_book = document.getElementById(b);
 			
 			//if book is visible, search in book title + author
 			if (cur_book.style.display != 'none' ) {
-				var compound_str = b.title + " " + b.author.firstName + " " + b.author.lastName;
+				var compound_str = myLib[b].title + myLib[b].authorFullName;
 				if (compound_str.toLowerCase().search(s_str.toLowerCase()) > -1) {
 					cur_book.style.display = 'flex';
 				} else { 
