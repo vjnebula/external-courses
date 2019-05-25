2ab3841bea5b9780a44c35639746ca763c07eed1
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-25 09:22:38 +0300

    Update addBookFromLocalFile.js

--- Library/utils/addBookFromLocalFile.js
+++ Library/utils/addBookFromLocalFile.js
@@ -1,4 +1,4 @@
-/* var getfiles = function(){
+var getfiles = function(){
 			
 			var f_input = document.getElementById('file-input');
 			var bookArr = f_input.files;
@@ -8,16 +8,10 @@
 				bookfile = bookArr[i];
 				TitleCut = bookfile.name.split(" by ");
 				bookUrl = window.URL.createObjectURL(bookfile);
-				var bk_id = myLib.length+1;
 				TitleCut[1] = TitleCut[1].replace(".png", " ");
-				ContentAdd(bk_id, TitleCut[0], ("by " + TitleCut[1]), bookUrl);
-				var newAuthor = TitleCut[1].split(" ");
-				var newBook = {"id":bk_id, "title":TitleCut[0],"author":{"firstName":newAuthor[0],"lastName":newAuthor[1]},"rating":3,"cost":0,"categories":["must_read","best","non_fiction"],"createdAt":1506943763424,"updatedAt":1528046197707,"image_url":bookUrl};
-				myLib.push(newBook);
+				createABook (Book, "addBook" + i, TitleCut[0], {}, 3, 0, ["must_read","best","non_fiction"], Date(), Date(), bookUrl);
+				myLib["addBook" + i].authorFullName = TitleCut[1];
+				ContentAdd(myLib["addBook" + i]);
 				newHistMesAdd("You added new book " + TitleCut[0]);
 			}
-}//getfiles */
-
-var getfiles = function(){
-	alert("under development!");
 }//getfiles
