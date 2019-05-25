16847ea2c63cc0955f450694139b72f7f5904329
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-25 09:22:22 +0300

    Update stars.controller.js

--- Library/controllers/stars.controller.js
+++ Library/controllers/stars.controller.js
@@ -1,8 +1,30 @@
 function setStarsEvent(){
-var starArr = document.getElementsByClassName('SVGstars');
-for (var s of starArr) {
-	s.onmouseover = setFill;
-	s.onmouseout = clear;
-	s.onclick = setRate;
-}
+	var starArr = document.getElementsByClassName('SVGstars');
+	for (var s of starArr) {
+		s.onmouseenter = setFill;
+		s.onmouseleave = clear;
+		s.onclick = setRate;
+	}
+}//setStarsEvent
+
+
+function setRating(rating, prnt) {
+	var cur_id = prnt.parentElement.parentElement.id;
+	var prntLngth = prnt.children.length;
+	if ( rating <= 0  ) {
+		for (var i = 0; i < prntLngth; i++) {
+			prnt.children[i].style.fill = '';
+			prnt.children[i].onmouseleave = clear;
+			prnt.children[i].onmouseenter = setFill;
+		}//for i
+	} else {
+		for (var i = 0; i < prntLngth; i++) {
+			prnt.children[i].onmouseleave = null;
+			prnt.children[i].onmouseenter = null;
+			if ( i < (rating*2)) {
+				prnt.children[i].style.fill = '#ffab00';
+			}
+		}
+	}
+	myLib[cur_id].newRating = rating;
 }
