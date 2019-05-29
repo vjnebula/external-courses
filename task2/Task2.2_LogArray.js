c38e5342f2169282bfe6945f7cb21d275a2b15aa
Yuriy <48429625+vjnebula@users.noreply.github.com>
2019-05-29 16:31:25 +0300

    deleted

--- task2/Task2.2_LogArray.js
+++
@@ -1,9 +0,0 @@
-function LogArray (Arr){
-    for (var val of Arr){
-        console.log(val);
-    }
-    console.log("Длинна массива "+Arr.length);
-}
-LogArray("Ann, Bill, Alice, Marry, Klod, Pol, Jack".split(", "));
-
-end = "end of script";
